5cfbb0b8c4f9e58260cf417219b5642d84e6a466
Javier Reyes <javier.reys@gmail.com>
2022-03-11 23:23:46 -0300

    chore(vite): :technologist: add plugins for auto import components and functions

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,37 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
+    },
+
+    plugins: [
+        vue(),
+
+        // https://github.com/antfu/unplugin-auto-import
+        AutoImport({
+            imports: ['vue', 'vue-router'],
+            dts: 'src/auto-imports.d.ts',
+        }),
+
+        // https://github.com/antfu/unplugin-vue-components
+        Components({
+            // allow auto load markdown components under `./src/components/`
+            extensions: ['vue', 'md'],
+            // allow auto import and register components used in markdown
+            include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+            dts: 'src/components.d.ts',
+        }),
+    ],
+
+    optimizeDeps: {
+        include: ['vue', 'vue-router'],
     },
 })

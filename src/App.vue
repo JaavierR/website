1def36cc73a534c507bde8910e07ec6763280017
Javier Reyes <javier.reys@gmail.com>
2022-03-14 19:32:18 -0300

    feat: :mag: add basic description meta tag

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+useHead({
+    title: 'Javier Reyes Website',
+    meta: [
+        {
+            name: 'description',
+            content:
+                'Young developer from Chile, interesting in web development and artificial intelligence. Also a teacher at USACH',
+        },
+    ],
+})
+</script>
+
 <template>
     <RouterView />
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
+import type { Owner } from '@/types/Response/Github'
+import axios from 'axios'
+
 useHead({
     title: 'Home',
 })
+
+const profile = ref<Owner | null>(null)
+
+const fetchGithub = async () => {
+    const response = (await axios.get('https://api.github.com/users/JaavierR'))
+        .data
+    profile.value = response
+}
+
+fetchGithub()
 
 const skills = [
     'JavaScript',
@@ -18,7 +31,23 @@ const skills = [
 </script>
 
 <template>
-    <div>
+    <div class="space-y-14">
+        <header class="my-16 px-4">
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Perspiciatis eligendi ipsum eveniet, eum asperiores ut
+                voluptates eos nesciunt, labore similique esse necessitatibus
+                aspernatur numquam commodi ab id earum, repellendus natus?
+            </p>
+            <div class="mb-4 rounded-full md:mb-0">
+                <img
+                    :src="profile?.avatar_url"
+                    alt="javier profile image"
+                    class="h-30 w-30 rounded-full md:h-40 md:w-40"
+                />
+            </div>
+        </header>
+
         <section id="technologies" class="mt-6">
             <h3
                 class="px-4 text-lg font-medium uppercase text-gray-400 dark:text-violet-400"
